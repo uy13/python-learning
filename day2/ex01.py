41b4036a5e9f3f6a732f75a90fa2a41a5eb1ebb7
uy13 <uydz2008@gmail.com>
2024-06-07 20:48:51 +0700

    create day02

---
+++ day2/ex01.py
@@ -0,0 +1,1 @@
+input("Nhap ten cua ban: ")
---
+++ day2/main.py
@@ -0,0 +1,20 @@
+# print(1, 2, 3, 4, sep=" $ ", end="..")
+# print(1000)
+# print("😍😍💚💚")
+# print('abc"')
+# print("I'm 16")
+
+# variables
+x = 1000
+print(x)
+
+name = "john"
+print(name)
+
+name = "jack"
+print(name)
+
+x = 100
+y = 10
+
+print(x + y)
